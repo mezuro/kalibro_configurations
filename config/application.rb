bcb834caeb124ff4a5f140d79db06ca946d8ebbd
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-10-20 13:50:26 -0200

    Skipping useless generators

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module KalibroConfigurations
 
     # For not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.template_engine :jbuilder
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
