1fd699b551505297dc59225fba592d3b21ebbe15
Diego Araújo <diegoamc90@gmail.com>
2014-10-24 15:14:18 -0200

    Included associations between reading groups and readings
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- spec/models/reading_group_spec.rb
+++ spec/models/reading_group_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ReadingGroup, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:readings)}
+  end
 end
