bd1f959cd2dbc235630e7551ad9eec47be01400e
Heitor Reis <marcheing@gmail.com>
2014-10-27 15:39:42 -0200

    Added test for metric configuration association
    Signed-off-by: Daniel Alves <danpaulalves@gmail.com>

--- spec/models/metric_configuration_spec.rb
+++ spec/models/metric_configuration_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MetricConfiguration, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:kalibro_configuration) }
+  end
 end
