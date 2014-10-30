38c5944c7611559d6a62a9c316dc6cf69ebf2c2b
Diego Araújo <diegoamc90@gmail.com>
2014-10-30 09:55:30 -0200

    WIP: Model validations for Metric.
    What is left to be done?
    - Test validations with conditionals.
    - Validates uniqueness of code in the scope of a kalibro_configuration,
      not in the scope of a metric_configuration.

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Metric, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:metric_configurations) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:code) }
+    it { is_expected.to validate_presence_of(:scope) }
+  end
 end
