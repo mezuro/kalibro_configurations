e53032a75442de880afd58fba843428f38ec375b
Diego Araújo <diegoamc90@gmail.com>
2014-10-30 08:38:14 -0200

    Model validations for Reading Group.

--- app/models/reading_group.rb
+++ app/models/reading_group.rb
@@ -1,3 +1,5 @@
 class ReadingGroup < ActiveRecord::Base
   has_many :readings
+
+  validates :name, presence: true
 end
--- spec/models/reading_group_spec.rb
+++ spec/models/reading_group_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ReadingGroup, :type => :model do
   describe 'associations' do
     it { is_expected.to have_many(:readings)}
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
