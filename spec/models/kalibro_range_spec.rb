c82f1c9ba81fa60877865548d215ede55ed4745c
Diego Araújo <diegoamc90@gmail.com>
2014-10-31 15:40:12 -0200

    Added validators on KalibroRanges
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- spec/models/kalibro_range_spec.rb
+++ spec/models/kalibro_range_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe KalibroRange, :type => :model do
     it { is_expected.to belong_to(:reading) }
     it { is_expected.to belong_to(:metric_configuration) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:beginning) }
+    it { is_expected.to validate_presence_of(:end) }
+    it { is_expected.to validate_presence_of(:metric_configuration) }
+    it { is_expected.to validate_presence_of(:reading) }
+    it { is_expected.to validate_uniqueness_of(:beginning).
+        scoped_to(:metric_configuration_id).with_message("Should be unique within a Metric Configuration") }
+  end
 end
