6a76ebb24e4d6369d1cdef17ebe94b943b2d650a
Diego Araújo <diegoamc90@gmail.com>
2014-10-31 17:11:11 -0200

    Cascade destroy for CompoundMetrics when destroying MetricConfiguration.

--- app/models/metric_configuration.rb
+++ app/models/metric_configuration.rb
@@ -4,7 +4,16 @@ class MetricConfiguration < ActiveRecord::Base
   belongs_to :kalibro_configuration
   belongs_to :metric
   has_many :kalibro_ranges, dependent: :destroy
+  before_destroy :destroy_compound_metric
 
   validates :metric, code_uniqueness: true
   validates :aggregation_form, :weight, :kalibro_configuration, :metric, presence: true
+
+  private
+
+  def destroy_compound_metric
+    if self.metric.type == "CompoundMetric"
+      self.metric.destroy
+    end
+  end
 end
