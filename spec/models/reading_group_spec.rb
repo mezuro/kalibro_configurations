6027bce9fbfad2b2f853564f42f2e7f5488903cd
Heitor Reis <marcheing@gmail.com>
2014-11-14 16:00:05 -0200

    Added uniqueness validator to Reading group
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- spec/models/reading_group_spec.rb
+++ spec/models/reading_group_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe ReadingGroup, :type => :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
