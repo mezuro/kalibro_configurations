c2c837f429a9200d3822e106e0f874d52f99be87
Diego Araújo <diegoamc90@gmail.com>
2015-02-03 16:03:11 -0200

    Created custom color validator
    Signed off by: Heitor Reis <marcheing@gmail.com>

---
+++ app/models/validators/color_validator.rb
@@ -0,0 +1,7 @@
+class ColorValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /^\h{6}$/
+      record.errors[attribute] << "Color must be hexadecimal"
+    end
+  end
+end
---
+++ spec/models/validators/color_validator_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'validators/color_validator'
+
+RSpec.describe ColorValidator, :type => :model do
+  describe 'methods' do
+    describe 'validate' do
+      context 'when the color is valid' do
+        subject { FactoryGirl.build(:reading, color: "a9F0b2") }
+        it 'is not expected to return an error' do
+          subject.save
+          expect(subject.errors["color"]).to be_empty
+        end
+      end
+      context 'when the color is not valid' do
+        subject { FactoryGirl.build(:reading, color: "aaa") }
+        it "is expected to return an error when color does not have exactly 6 digits" do
+          subject.save
+          expect(subject.errors["color"]).to eq(["Color must be hexadecimal"])
+        end
+
+        subject { FactoryGirl.build(:reading, color: "GGGGGG") }
+        it "is expected to return an error when color is not hexadecimal" do
+          subject.save
+          expect(subject.errors["color"]).to eq(["Color must be hexadecimal"])
+        end
+      end
+    end
+  end
+end
