02c21352a2d5c7f5a62f18c81daad1d28d40f414
Heitor Reis <marcheing@gmail.com>
2015-02-05 13:00:49 -0200

    Aggregation form only validates when the metric snapshot is native
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/models/metric_configuration.rb
+++ app/models/metric_configuration.rb
@@ -3,10 +3,11 @@ class MetricConfiguration < ActiveRecord::Base
   belongs_to :metric_snapshot, dependent: :destroy
   has_many :kalibro_ranges, dependent: :destroy
 
-  validates :aggregation_form, :weight, :kalibro_configuration, :metric_snapshot, presence: true
+  validates :weight, :kalibro_configuration, :metric_snapshot, presence: true
   validates :weight, numericality: { greater_than: 0 }
   validates :metric_snapshot, uniqueness: { scope: :kalibro_configuration_id,
     message: "Should be unique within a Kalibro Configuration" }
+  validates :aggregation_form, presence: true, if: "native_metric_snapshot?"
 
   accepts_nested_attributes_for :metric_snapshot
 
@@ -24,5 +25,9 @@ class MetricConfiguration < ActiveRecord::Base
     end
 
     return json
+  end
+
+  def native_metric_snapshot?
+    !metric_snapshot.nil? && metric_snapshot.type == "NativeMetricSnapshot"
   end
 end
