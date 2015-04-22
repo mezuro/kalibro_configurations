6fab7f7dd78e59f4b2c8db3b78b36058f94b45a4
Heitor Reis <marcheing@gmail.com>
2015-04-22 11:46:20 -0300

    Added configuration for Flog
    Signed off by: Daniel Miranda <danielkza2@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 kalibro_configuration = KalibroConfiguration.create(name: "First Configuration", description: "Configuration proposed as starting point for C, C++ and Java. From Morais' master text.")
+ruby_configuration = KalibroConfiguration.create(name: "Ruby Configuration", description: "Example Ruby Configuration")
 
 reading_group = ReadingGroup.create(name: "Scholar", description: "This group imitates school grades and attributes generic labels from 'Terrible' (grade 0) to 'Excellent' (grade 10).\nThe colors vary in wave length with the grades, ranging from red to green (between them are tones of orange and yellow).")
 
@@ -128,3 +129,19 @@ KalibroRange.create([
   { beginning: 51, end: Float::INFINITY, comments: "Complexidade estrutural é um fator de risco. Verifique se a classe pode ser dividida em classes menores e mais coesas. Classes frutos dessa divisão tendem a ser menos acopladas.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
 ])
 
+###############################################################################
+
+metric_snapshot = NativeMetricSnapshot.create(name: "Pain", description: "Flog mede a tortuosidade do código-fonte. Quanto mais doloroso e difícil de testar, maior a pontuação, baseando-se na métrica ABC e boas práticas de Ruby.", code: "flog", metric_collector_name: "MetricFu", scope: "METHOD")
+
+metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 3.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: ruby_configuration.id)
+
+KalibroRange.create([
+  { beginning: 0, end: 10, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
+  { beginning: 10, end: 20, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
+  { beginning: 20, end: 40, comments: "", reading_id: readings[-4].id, metric_configuration_id: metric_configuration.id },
+  { beginning: 40, end: 60, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
+  { beginning: 60, end: 100, comments: "", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id },
+  { beginning: 100, end: Float::INFINITY, comments: "", reading_id: readings[-9].id, metric_configuration_id: metric_configuration.id }
+])
+
+###############################################################################
