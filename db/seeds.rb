d703787d5a2e94b380b3e67d0ae8136bdbe6e369
Heitor Reis <marcheing@gmail.com>
2015-04-27 14:13:36 -0300

    Refactored readings parameters for seeds
    Signed-off-by: Pedro Scocco <pedroscocco@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,13 @@ readings = Reading.create([
   { label: "Excellent", grade: 10, color: "00FF00", reading_group_id: reading_group.id }
 ])
 
+terrible = readings.first.id
+worrying = readings[2].id
+regular = readings[4].id
+satisfactory = readings[5].id
+good = readings[6].id
+excellent = readings.last.id
+
 ###############################################################################
 ########################### Java/C/C++ Configuration ##########################
 ###############################################################################
@@ -35,10 +42,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Afferent Connections per Cl
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 2.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 2, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 2, end: 7, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 7, end: 15, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 15, end: Float::INFINITY, comments: "A classe possui muitas dependências. Tente seguir o princípio de responsabilidade única. Talvez seja necessário reduzir a interface das classes utilizadas.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 2, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 2, end: 7, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 7, end: 15, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 15, end: Float::INFINITY, comments: "A classe possui muitas dependências. Tente seguir o princípio de responsabilidade única. Talvez seja necessário reduzir a interface das classes utilizadas.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -48,10 +55,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Average Cyclomatic Complexi
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 2.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 3, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 3, end: 5, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 5, end: 7, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 7, end: Float::INFINITY, comments: "Os métodos estão muito “pesados”. Tente quebrá-los em métodos menores e menos complexos. Evite longos “switches”, ou vários laços aninhados no mesmo método.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 3, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 3, end: 5, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 5, end: 7, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 7, end: Float::INFINITY, comments: "Os métodos estão muito “pesados”. Tente quebrá-los em métodos menores e menos complexos. Evite longos “switches”, ou vários laços aninhados no mesmo método.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -61,10 +68,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Average Method Lines of Cod
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 1.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 8, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 8, end: 19, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 19, end: 37, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 37, end: Float::INFINITY, comments: "Os métodos estão muito longos. Tente quebrá-los em métodos menores, cada um no seu nível de abstração. Forte candidato a novo método é o código em maior aninhamento.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 8, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 8, end: 19, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 19, end: 37, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 37, end: Float::INFINITY, comments: "Os métodos estão muito longos. Tente quebrá-los em métodos menores, cada um no seu nível de abstração. Forte candidato a novo método é o código em maior aninhamento.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -74,10 +81,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Average Number of Parameter
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 1.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 2, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 2, end: 3, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 3, end: 5, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 5, end: Float::INFINITY, comments: "Os métodos estão recebendo muitos parâmetros. Conjuntos de parâmetros parecidos sugerem a criação de uma classe contendo esses dados, e transferência dos métodos para essa classe.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 2, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 2, end: 3, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 3, end: 5, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 5, end: Float::INFINITY, comments: "Os métodos estão recebendo muitos parâmetros. Conjuntos de parâmetros parecidos sugerem a criação de uma classe contendo esses dados, e transferência dos métodos para essa classe.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -87,10 +94,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Depth of Inheritance Tree",
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 1.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 2, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 2, end: 4, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 4, end: 6, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 6, end: Float::INFINITY, comments: "Talvez o mecanismo de herança esteja sendo utilizado em demasia. Sempre que fizer sentido, prefira composição à herança.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 2, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 2, end: 4, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 4, end: 6, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 6, end: Float::INFINITY, comments: "Talvez o mecanismo de herança esteja sendo utilizado em demasia. Sempre que fizer sentido, prefira composição à herança.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -100,10 +107,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Number of Methods", descrip
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 1.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 10, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 10, end: 17, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 17, end: 27, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 27, end: Float::INFINITY, comments: "Muitos métodos em uma só classe a torna mais difícil de entender. Talvez o princípio da responsabilidade única esteja sendo violado. Talvez alguns métodos não sejam necessários.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 10, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 10, end: 17, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 17, end: 27, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 27, end: Float::INFINITY, comments: "Muitos métodos em uma só classe a torna mais difícil de entender. Talvez o princípio da responsabilidade única esteja sendo violado. Talvez alguns métodos não sejam necessários.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -113,10 +120,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Number of Public Attributes
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 1.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 1, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 1, end: 2, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 2, end: 3, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 3, end: Float::INFINITY, comments: "Em linguagens orientadas a objetos, atributos públicos são uma aberração e devem ser evitados ao máximo. Esconda os atributos e forneça métodos para controlar o acesso a eles.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 1, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 1, end: 2, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 2, end: 3, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 3, end: Float::INFINITY, comments: "Em linguagens orientadas a objetos, atributos públicos são uma aberração e devem ser evitados ao máximo. Esconda os atributos e forneça métodos para controlar o acesso a eles.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -126,10 +133,10 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Structural Complexity", des
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 4.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: kalibro_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 12, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 12, end: 28, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 28, end: 51, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 51, end: Float::INFINITY, comments: "Complexidade estrutural é um fator de risco. Verifique se a classe pode ser dividida em classes menores e mais coesas. Classes frutos dessa divisão tendem a ser menos acopladas.", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 12, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 12, end: 28, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 28, end: 51, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 51, end: Float::INFINITY, comments: "Complexidade estrutural é um fator de risco. Verifique se a classe pode ser dividida em classes menores e mais coesas. Classes frutos dessa divisão tendem a ser menos acopladas.", reading_id: worrying, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
@@ -143,12 +150,12 @@ metric_snapshot = NativeMetricSnapshot.create(name: "Pain", description: "Flog m
 metric_configuration = MetricConfiguration.create(metric_snapshot_id: metric_snapshot.id, weight: 3.0, aggregation_form: "AVERAGE", reading_group_id: reading_group.id, kalibro_configuration_id: ruby_configuration.id)
 
 KalibroRange.create([
-  { beginning: 0, end: 10, comments: "", reading_id: readings.last.id, metric_configuration_id: metric_configuration.id },
-  { beginning: 10, end: 20, comments: "", reading_id: readings[-3].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 20, end: 40, comments: "", reading_id: readings[-4].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 40, end: 60, comments: "", reading_id: readings[-5].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 60, end: 100, comments: "", reading_id: readings[-7].id, metric_configuration_id: metric_configuration.id },
-  { beginning: 100, end: Float::INFINITY, comments: "", reading_id: readings[-9].id, metric_configuration_id: metric_configuration.id }
+  { beginning: 0, end: 10, comments: "", reading_id: excellent, metric_configuration_id: metric_configuration.id },
+  { beginning: 10, end: 20, comments: "", reading_id: good, metric_configuration_id: metric_configuration.id },
+  { beginning: 20, end: 40, comments: "", reading_id: satisfactory, metric_configuration_id: metric_configuration.id },
+  { beginning: 40, end: 60, comments: "", reading_id: regular, metric_configuration_id: metric_configuration.id },
+  { beginning: 60, end: 100, comments: "", reading_id: worrying, metric_configuration_id: metric_configuration.id },
+  { beginning: 100, end: Float::INFINITY, comments: "", reading_id: terrible, metric_configuration_id: metric_configuration.id }
 ])
 
 ###############################################################################
