d477932164b8add2d3e6d242e95e77079d7e84ab
MaxAlmeida <llewxam100@hotmail.com>
2015-05-14 14:42:54 -0300

    refactoring statistic test
    Signed-off-by: MaxAlmeida <llewxam100@hotmail.com>
    Signed-off-by: Parley Martins <parleypachecomartins@gmail.com>

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -1,49 +1,53 @@
 require 'rails_helper'
 RSpec.describe StatisticsController, type: :controller do
 
-  describe "GET #metric_percentage for an used metric" do
-    
-    before :each do
-      MetricConfiguration.expects(:all).returns([FactoryGirl.build(:metric_configuration),
-        FactoryGirl.build(:metric_configuration),FactoryGirl.build(:metric_configuration)])
-      MetricSnapshot.expects(:where).returns([FactoryGirl.build(:metric_snapshot)])
-      get :metric_percentage, metric_code: 'loc', format: :json
+  describe "#metric_percentage" do
+    let(:all_metrics) {[FactoryGirl.build(:metric_configuration),FactoryGirl.build(:metric_configuration),
+      FactoryGirl.build(:metric_configuration)]}
+    let(:metric_snapshot) {[FactoryGirl.build(:metric_snapshot)]}
+    let(:no_metric_configuration) {[]}
+    let(:no_metric_snapshot) {[]}
+
+    context "when metric has been used" do
+      before :each do
+        MetricConfiguration.expects(:all).returns(all_metrics)
+        MetricSnapshot.expects(:where).returns(metric_snapshot)
+        get :metric_percentage, metric_code: 'loc', format: :json
+      end
+
+      it {should respond_with(:success)}
+
+      it "returns use percentage of the selected metric" do
+        expect(JSON.parse(response.body)).to eq(JSON.parse({ metric_percentage: 33.33 }.to_json))
+      end
     end
 
-    it {should respond_with(:success)}
+    context "when no metric configuration has been created" do
+      before :each do
+        MetricConfiguration.expects(:all).returns(no_metric_configuration)
+        MetricSnapshot.expects(:where).returns(no_metric_snapshot)
+        get :metric_percentage, metric_code: 'loc', format: :json
+      end
 
-    it "returns the statistics of an existing metric" do  
-      expect(JSON.parse(response.body)).to eq(JSON.parse({ metric_percentage: 33.33 }.to_json))
+      it {should respond_with(:success)}
+
+      it "returns zero percentage" do
+        expect(JSON.parse(response.body)).to eq(JSON.parse({ metric_percentage: 0.0 }.to_json))
+      end
+    end
+
+    context "when metric was not used yet" do
+      before :each do
+        MetricConfiguration.expects(:all).returns(all_metrics)
+        MetricSnapshot.expects(:where).returns(no_metric_snapshot)
+        get :metric_percentage, metric_code: 'loc', format: :json
+      end
+
+      it {should respond_with(:success)}
+
+      it "returns zero percentage" do
+        expect(JSON.parse(response.body)).to eq(JSON.parse({ metric_percentage: 0.0 }.to_json))
+      end
     end
   end
-
-  describe "GET #metric_percentage with no metric configurations in the database" do
-    before :each do
-      MetricConfiguration.expects(:all).returns([])
-      MetricSnapshot.expects(:where).returns([])
-      get :metric_percentage, metric_code: 'loc', format: :json
-    end
-
-    it {should respond_with(:success)}
-
-    it "returns zero for empty metric configurations database" do  
-      expect(JSON.parse(response.body)).to eq(JSON.parse({ metric_percentage: 0.0 }.to_json))
-    end
-  end
-
-  describe "GET #metric_percentage for a metric that has not been used yet" do
-    before :each do
-      MetricConfiguration.expects(:all).returns([FactoryGirl.build(:metric_configuration),
-        FactoryGirl.build(:metric_configuration),FactoryGirl.build(:metric_configuration)])
-      MetricSnapshot.expects(:where).returns([])
-      get :metric_percentage, metric_code: 'loc', format: :json
-    end
-
-    it {should respond_with(:success)}
-
-    it "returns zero percent for metric not used yet" do  
-      expect(JSON.parse(response.body)).to eq(JSON.parse({ metric_percentage: 0.0 }.to_json))
-    end
-  end
-  
 end
