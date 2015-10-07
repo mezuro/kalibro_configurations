01c3a7dc99af8c771b63a1153f6210d98002592d
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-10-07 14:10:14 -0300

    Remove unnecessary stub for kalibro range spec
    This stub prevented the actual spec to run.
    
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- spec/models/kalibro_range_spec.rb
+++ spec/models/kalibro_range_spec.rb
@@ -21,12 +21,6 @@ RSpec.describe KalibroRange, :type => :model do
     context 'with invalid beginning or end' do
       subject { FactoryGirl.build(:kalibro_range_with_id) }
 
-      before :each do
-        # Make sure the uniqueness in the Metric Configuration scope isn't triggered, since
-        # we only want to test validations that deal exclusively with the range values
-        ActiveRecord::Validations::UniquenessValidator.any_instance.stubs(:validate_each)
-      end
-
       @cases = [
           [Float::INFINITY, Float::INFINITY],
           [Float::INFINITY, -Float::INFINITY],
