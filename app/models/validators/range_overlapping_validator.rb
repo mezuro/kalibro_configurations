2ea22932d88d050959c79565c1ce34a4ab4f2348
Heitor Reis <marcheing@gmail.com>
2016-03-28 14:14:45 -0300

    Refactored range overlapping validator

--- app/models/validators/range_overlapping_validator.rb
+++ app/models/validators/range_overlapping_validator.rb
@@ -8,10 +8,30 @@ class RangeOverlappingValidator < ActiveModel::Validator
   end
 
   def overlaps?(range1, range2)
-    return true if range1.beginning >= range2.beginning && range1.beginning < range2.end
-    return true if range1.end > range2.beginning && range1.end <= range2.end
-    return true if range1.beginning >= range2.beginning && range1.end <= range2.end
-    return true if range1.beginning <= range2.beginning && range1.end >= range2.end
+    return true if range_1_begins_inside_range_2(range1, range2)
+    return true if range_1_ends_inside_range_2(range1, range2)
+    return true if range_1_begins_and_ends_inside_range_2(range1, range2)
+    return true if range_1_begins_and_ends_inside_range_2(range2, range1)
     false
+  end
+
+  private
+
+  # Range 1 - |------| <>    |------|
+  # Range 2 - |----|   <> |----|
+  def range_1_begins_inside_range_2(range1, range2)
+    range1.beginning >= range2.beginning && range1.beginning < range2.end
+  end
+
+  # Range 1 - |------|   <> |------|
+  # Range 2 -   |------| <>   |----|
+  def range_1_ends_inside_range_2(range1, range2)
+    range1.end > range2.beginning && range1.end <= range2.end
+  end
+
+  # Range 1 -   |---|  <> |------|
+  # Range 2 - |------| <> |------|
+  def range_1_begins_and_ends_inside_range_2(range1, range2)
+    range1.beginning >= range2.beginning && range1.end <= range2.end
   end
 end
